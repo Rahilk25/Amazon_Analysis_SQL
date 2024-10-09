f2527c97ae6dc7193a659b8f64dcda533e01e570
Rahilk25 <140850600+Rahilk25@users.noreply.github.com>
2024-10-09 11:13:44 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE sales(
-					id int PRIMARY KEY,
-					order_date date,
-					customer_name VARCHAR(25),
-					state VARCHAR(25),
-					category VARCHAR(25),
-					sub_category VARCHAR(25),
-					product_name VARCHAR(255),
-					sales FLOAT,
-					quantity INT,
-					profit FLOAT
-					);
+		id int PRIMARY KEY,
+		order_date date,
+		customer_name VARCHAR(25),
+		state VARCHAR(25),
+		category VARCHAR(25),
+		sub_category VARCHAR(25),
+		product_name VARCHAR(255),
+		sales FLOAT,
+		quantity INT,
+		profit FLOAT
+);
